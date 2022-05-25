47c11508f5c69427299cfefeab7fbf30e0b987bc
Chumeng Xu <ChumengXu@users.noreply.github.com>
2022-05-25 17:38:59 +0100

    Add function to add two integers
    Add function to add two integers -- takes two integers, sum it up, print the output and return the result.

--- course_exercises/writing_a_function.R
+++ course_exercises/writing_a_function.R
@@ -5,6 +5,8 @@
 # 3) Add an optional argument called "verbose" to your function that prints: 
 #     "Calculating the sum for 2 integers; hang tight!" (even better if you include the 2 integers)
 
-SumTwoInt <- function() {
-  ...
+SumTwoInt <- function(x,y) {
+  z <- x + y
+  print(z)
+  z
 }
